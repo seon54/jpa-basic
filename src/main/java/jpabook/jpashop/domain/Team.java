bb443e887845c42d9825d60dde19b46e7e15136b
shpark <jlk@shpark.local>
2020-11-21 18:39:42 +0900

    일대일 [1:1]

--- src/main/java/jpabook/jpashop/domain/Team.java
+++ src/main/java/jpabook/jpashop/domain/Team.java
@@ -1,8 +1,9 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Team {
@@ -11,10 +12,6 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
-
-    @OneToMany
-    @JoinColumn(name = "TEAM_ID")
-    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -32,7 +29,4 @@ public class Team {
         this.name = name;
     }
 
-    public List<Member> getMembers() {
-        return members;
-    }
 }
