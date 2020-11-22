5e4fb808a1611e52cbd8cbde104d93013f5f9228
shpark <jlk@shpark.local>
2020-11-22 19:51:28 +0900

    프록시

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,13 +1,10 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
+import org.hibernate.Hibernate;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.time.LocalDateTime;
+import javax.persistence.*;
+
 
 public class JpaMain {
 
@@ -18,15 +15,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("Kim");
+            Member member1 = new Member();
+            member1.setName("user1");
+            em.persist(member1);
 
-            em.persist(book);
+            em.flush();
+            em.clear();
+
+            Member m1 = em.getReference(Member.class, member1.getId());
+            System.out.println("m1 = " + m1.getClass());
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(m1));
+            Hibernate.initialize(m1);
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(m1));
+
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.clear();
         }
