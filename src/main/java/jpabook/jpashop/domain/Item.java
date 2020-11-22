763abe00e54f41a78cf45a7608c58e71b5d36a33
seon54 <seonhyeongpark54@gmail.com>
2020-11-22 18:53:13 +0900

    실전 예제 4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -3,7 +3,7 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
 public abstract class Item extends BaseEntity {
 
