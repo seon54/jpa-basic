b195335e50b5c44b8f46aaf83c7591a38b06568b
seon54 <seonhyeongpark54@gmail.com>
2020-11-23 18:27:30 +0900

    영속성 전이(CASCADE)와 고아 객체

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,11 +1,12 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Team;
-import org.hibernate.Hibernate;
+import jpabook.jpashop.domain.Child;
+import jpabook.jpashop.domain.Parent;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 
 public class JpaMain {
@@ -17,33 +18,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setName("user1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setName("user2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             em.flush();
             em.clear();
 
-//            Member m1 = em.find(Member.class, member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m left join fetch m.team", Member.class)
-                    .getResultList();
-
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
             tx.commit();
         } catch (Exception e) {
