9eb6357f29cd130c6f8c85cb070379fd0c2db9ce
seon54 <seonhyeongpark54@gmail.com>
2020-11-25 23:44:23 +0900

    실전예제 5- 연관관계 정리

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+}
