095b003dfe8b629b9912e0f006b980a6419f97ef
shpark <jlk@shpark.local>
2020-11-26 13:52:15 +0900

    값 타입의 비교

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -19,18 +19,7 @@ public class JpaMain {
         try {
             Address address = new Address("city", "street", "10");
 
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(address);
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-
-            Member member2 = new Member();
-            member2.setName("userB");
-            member2.setAddress(copyAddress);
-
-            em.persist(member);
-            em.persist(member2);
 
             tx.commit();
         } catch (Exception e) {
