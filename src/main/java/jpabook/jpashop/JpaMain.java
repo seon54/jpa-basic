abce29b53b2937dcfaaa9cf0df8fd5a63db69265
seon54 <seonhyeongpark54@gmail.com>
2020-11-26 19:12:38 +0900

    값 타입 컬렉션

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,11 +1,15 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.AddressEntity;
+import jpabook.jpashop.domain.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 
 public class JpaMain {
@@ -17,8 +21,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city", "street", "10");
+            Member member = new Member();
+            member.setName("A");
+            member.setAddress(new Address("Seoul", "Gangnam", "123"));
 
+            member.getFavoriteFoods().add("pasta");
+            member.getFavoriteFoods().add("pizza");
+            member.getFavoriteFoods().add("bread");
+
+            member.getAddressHistory().add(new AddressEntity("city1", "street1", "123"));
+            member.getAddressHistory().add(new AddressEntity("city1", "street1", "123"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("===============================================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            Address a = findMember.getAddress();
+            findMember.setAddress(new Address("Incheon", a.getStreet(), a.getZipcode()));
+
+            findMember.getFavoriteFoods().remove("pizza");
+            findMember.getFavoriteFoods().add("korean food");
 
 
             tx.commit();
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,7 +3,9 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -28,6 +30,19 @@ public class Member {
 
     @Embedded
     private Period period;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -75,5 +90,21 @@ public class Member {
 
     public void setPeriod(Period period) {
         this.period = period;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
