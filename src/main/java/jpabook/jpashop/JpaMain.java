fd14c342af2de7f2fa6a0ebf56c2098cb4965b7f
shpark <jlk@shpark.local>
2020-11-27 14:41:37 +0900

    프로젝션(SELECT)

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.MemberDTO;
+import jpabook.jpashop.domain.Team;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 public class JpaMain {
@@ -18,10 +22,32 @@ public class JpaMain {
             member.setUsername("userA");
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member as m where m.username = :username", Member.class)
-                    .setParameter("username", "userA")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
+            em.flush();
+            em.clear();
+
+            List resultList = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+            System.out.println("result[0] = " + result[0]);
+            System.out.println("result[1] = " + result[1]);
+
+            List<Object[]> resultList2 = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+
+            for (Object[] objects : resultList2) {
+                System.out.println("objects[0] = " + objects[0]);
+                System.out.println("objects[1] = " + objects[1]);
+            }
+
+            List<MemberDTO> dtoList = em.createQuery("select new jpabook.jpashop.domain.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            for (MemberDTO memberDTO : dtoList) {
+                System.out.println("memberDTO = " + memberDTO.getUsername());
+                System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+            }
 
             tx.commit();
         } catch (Exception e) {
