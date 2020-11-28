0a578bcc29c942f48573fae2cffaa51ad3bce922
seon54 <seonhyeongpark54@gmail.com>
2020-11-28 14:37:31 +0900

    join fetch - 기본

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -23,14 +23,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("teamA");
+            em.persist(team1);
 
             Member member = new Member();
             member.setUsername("member");
             member.setAge(70);
-            member.changeTeam(team);
+            member.changeTeam(team1);
             member.setType(ADMIN);
             em.persist(member);
 
@@ -45,35 +45,58 @@ public class JpaMain {
             member2.setType(MemberType.USER);
             em.persist(member2);
 
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setAge(10);
+            member3.changeTeam(team2);
+            member3.setType(MemberType.USER);
+            em.persist(member3);
+
             em.flush();
             em.clear();
 
-            // 단일 값 연관 경로: 묵시적 내부 조인 발생, 탐색 O
-            String query = "select m.team.name from Member m";
-            List<String> resultList = em.createQuery(query, String.class)
-                    .getResultList();
+            // 묵시적 join
+            System.out.println("================================================================");
+            String query1 = "select m from Member m";
+            List<Member> results1 = em.createQuery(query1, Member.class).getResultList();
 
-            for (String s : resultList) {
-                System.out.println("s = " + s);
+            for (Member member1 : results1) {
+                System.out.println("member = " + member1 + " / team1: " + member1.getTeam().getName());
             }
 
-            // 컬렉션 값 연관 경로: 묵시적 내부 조인 발생, 탐색 X
-            String query2 = "select t.members from Team t";
-            Collection results2 = em.createQuery(query2, Collection.class)
-                    .getResultList();
+            // join fetch
+            System.out.println("============================ join fetch ====================================");
+            String query2 = "select m from Member m join fetch m.team";
+            List<Member> results2 = em.createQuery(query2, Member.class).getResultList();
 
-            for (Object s : results2) {
-                System.out.println("s = " + s);
+            for (Member r : results2) {
+                System.out.println("member = " + r.getUsername() + " / team1 = " + r.getTeam());
             }
 
-            // 명시적 조인
-            String query3 = "select m.username from Team t join t.members m";
-            List<String> results3 = em.createQuery(query3, String.class)
-                    .getResultList();
+            // join fetch(collection)
+            System.out.println("============================== join fetch (collection) ==================================");
+            String query3 = "select t From Team t join fetch t.members";
+            List<Team> results3 = em.createQuery(query3, Team.class).getResultList();
 
-            for (String s : results3) {
-                System.out.println("s = " + s);
+            for (Team team : results3) {
+                System.out.println("team = " + team.getName());
+                for (Member teamMember : team.getMembers()) {
+                    System.out.println("--> teamMember = " + teamMember.getUsername());
+                }
             }
+
+            // join fetch & distinct
+            System.out.println("============================== join fetch & distinct ==================================");
+            String query4 = "select distinct t From Team t join fetch t.members";
+            List<Team> results4 = em.createQuery(query4, Team.class).getResultList();
+
+            for (Team team : results4) {
+                System.out.println("team = " + team.getName());
+                for (Member teamMember : team.getMembers()) {
+                    System.out.println("--> teamMember = " + teamMember.getUsername());
+                }
+            }
+
 
             tx.commit();
         } catch (Exception e) {
